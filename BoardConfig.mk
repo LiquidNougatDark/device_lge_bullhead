ef30cf8c3837b82d52fb0e378241592192f589c1
t-rex600 <fisch890@gmail.com>
2017-04-14 12:53:20 +0200

    Bullhead: Switch DespairFactor Kernel and GCC 6.x

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,10 +31,9 @@ ENABLE_CPUSETS := true
 TARGET_NO_BOOTLOADER := true
 
 # Inline kernel building
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_GCC_VERSION_ARM64 := 6.x
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
-TARGET_KERNEL_CONFIG := pure_defconfig
+TARGET_KERNEL_CONFIG := phasma_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 BOARD_KERNEL_BASE        := 0x00000000
